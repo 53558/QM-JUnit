ef27fa661254ced8136f61eac9f943bd8c31ee13
53558 <32450287+53558@users.noreply.github.com>
2017-10-09 10:21:21 +0200

    testGetAge() runs Successfully

--- src/com/qmjunit/example/Person.java
+++ src/com/qmjunit/example/Person.java
@@ -1,5 +1,9 @@
 package com.qmjunit.example;
 
 public class Person {
-
+	
+	public int getAge() { 
+		return 0; 
+	}
+	
 }
--- test/com/qmunit/testexample/TC_Person.java
+++ test/com/qmunit/testexample/TC_Person.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.qmjunit.example.Person;
+
 public class TC_Person {
 
 	protected Person person;
